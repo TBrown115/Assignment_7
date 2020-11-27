7ec9c6745d7bf4e34affbd3b0fab8215d8352961
TBrown115 <70948046+TBrown115@users.noreply.github.com>
2020-11-27 15:38:58 +0200

    first commit

---
+++ Assignment7/Assignment7/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new TabbedPage1());
+            navPage.BarBackgroundColor = Color.FromHex("075E54");
+
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
+
